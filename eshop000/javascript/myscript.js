0845ca8b25a0222b2b3ca0935c0005578bcff65d
iordanis89 <45898511+iordanis89@users.noreply.github.com>
2022-08-03 16:17:49 +0300

    Delete eshop000 directory

--- eshop000/javascript/myscript.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('nav-ul');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
